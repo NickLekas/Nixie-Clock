e98cf1d76d0b53340fa5767fb56f77373771ad41
NickLekas <apollo2.95@hotmail.com>
2019-09-13 18:49:39 -0700

    Changed variable names

--- src/main.cpp
+++ src/main.cpp
@@ -8,19 +8,19 @@
 #include <config.h>
 #include <time.h>
 
-void getGPSTime(int &seconds, int &minutes, int &hours); //reads the time data from the gps and applies the time zone
-void getRTCTime(int &minutes, int &hours); //reads the time data from the RTC
+void getGPSTime(int &seconds, int &minute, int &hour); //reads the time data from the gps and applies the time zone
+void getRTCTime(int &minute, int &hour); //reads the time data from the RTC
 void setRTCTime(); //sets the RTC time using the GPS
-void twelveHour(int &hours); //converts the time from 24hr to 12hr
+void twelveHour(int &hour); //converts the time from 24hr to 12hr
 void startup(); //cyles the tubes and displays the current time at the end
 void updateRTC();
 void cycleDisplay(int &delaySpeed); //cycles the tubes thorugh all numbers
-void nixieDisplayMinutes(int a, int b, int c, int d, int value); //outputs binary to the minutes nixie drivers
-void nixieDisplayHours(int a, int b, int c, int d, int value); //outputs binary to the hours nixie drivers
+void nixieDisplayMinutes(int a, int b, int c, int d, int value); //outputs binary to the minute nixie drivers
+void nixieDisplayHours(int a, int b, int c, int d, int value); //outputs binary to the hourr nixie drivers
 
 //sets up the PCF8574 GPIO extenders
-PCF8574 minutes(0x20);
-PCF8574 hours(0x21);
+PCF8574 minute(0x20);
+PCF8574 hour(0x21);
 
 //sets up the real time clock
 RTC_DS3231 rtc;
@@ -41,11 +41,11 @@ void setup() {
   //sets all the pins of the PCF8574 I/O expanders to outputs for cntrolling the nixie drivers
   for (i = 0; i < 8; i++)
   {
-    minutes.pinMode(i, OUTPUT);
-    hours.pinMode(i, OUTPUT);
+    minute.pinMode(i, OUTPUT);
+    hour.pinMode(i, OUTPUT);
   }
-  minutes.begin();
-  hours.begin();
+  minute.begin();
+  hour.begin();
 
   //sets the display to 0
   nixieDisplayMinutes(onesA, onesB, onesC, onesD, 0);
@@ -64,7 +64,7 @@ void setup() {
 }
 
 void loop() {
-  int minutes, hours;
+  int minute, hour;
   int minOnes, minTens, hourOnes, hourTens;
   int last = -1, loop = 0;
 
@@ -72,23 +72,23 @@ void loop() {
 
   while (true) {
     //pulls time from the RTC
-    getRTCTime(minutes, hours);
+    getRTCTime(minute, hour);
 
     //checks if the time has changed from the last time get
-    if (last != minutes) {
+    if (last != minute) {
       //parses the minute data for the tens and ones digits
-      minOnes = minutes % 10;
-      minTens = (minutes - minOnes) / 10;
+      minOnes = minute % 10;
+      minTens = (minute - minOnes) / 10;
 
       //parses the hour data for the tens and ones digits
-      hourOnes = hours % 10;
-      hourTens = (hours - hourOnes) / 10;
+      hourOnes = hour % 10;
+      hourTens = (hour - hourOnes) / 10;
 
-      //writes the time data out to the minutes gpio expander
+      //writes the time data out to the minute gpio expander
       nixieDisplayMinutes(onesA, onesB, onesC, onesD, minOnes);
       nixieDisplayMinutes(tensA, tensB, tensC, tensD, minTens);
 
-      //writes the time data out to the hours gpio expander
+      //writes the time data out to the hour gpio expander
       nixieDisplayHours(onesA, onesB, onesC, onesD, hourOnes);
       nixieDisplayHours(tensA, tensB, tensC, tensD, hourTens);
 
@@ -104,7 +104,7 @@ void loop() {
       #endif
 
       //keeps track of the previous minute to compare with the next gps read cycle
-      last = minutes;
+      last = minute;
 
       //syncs the time updates to be every 60 seconds on the first loop and then sleeps the arduino ~60 seconds
       if(loop < 1){
@@ -118,7 +118,7 @@ void loop() {
 }
 
 //pulls 24hr UTC time from the GPS satellites
-void getGPSTime(int &seconds, int &minutes, int &hours) {
+void getGPSTime(int &seconds, int &minute, int &hour) {
   #ifdef GENERAL_DEBUG
     Serial.println("Waiting for GPS fix");
   #endif
@@ -148,8 +148,8 @@ void getGPSTime(int &seconds, int &minutes, int &hours) {
   #endif
 
   //gets the time from the new gps data
-  minutes = gps.minute;
-  hours = gps.hour;
+  minute = gps.minute;
+  hour = gps.hour;
   seconds = gps.seconds;
 
   return;
@@ -157,32 +157,32 @@ void getGPSTime(int &seconds, int &minutes, int &hours) {
 
 //RTC time is stored as 24hr UTC time
 void setRTCTime() {
-  int seconds, minutes, hours;
+  int seconds, minute, hour;
 
   //gets the time from the GPS
-  getGPSTime(seconds, minutes, hours);
+  getGPSTime(seconds, minute, hour);
 
   //stores the GPS time in the RTC, date is set to the start of UNIX time
-  rtc.adjust(DateTime(1970, 1, 1, hours, minutes, seconds));
+  rtc.adjust(DateTime(1970, 1, 1, hour, minute, seconds));
 
   return;
 }
 
 //Pulls the time from the RTC and converts it to 
-void getRTCTime(int &minutes, int &hours) {
+void getRTCTime(int &minute, int &hour) {
   bool update = true;
 
   //gets the current time from the RTC
   DateTime now = rtc.now();
 
   //pulls the RTC time
-  minutes = now.minute();
-  hours = now.hour();
+  minute = now.minute();
+  hour = now.hour();
 
   //converts from UTC to the desired time zone
-  hours += TIME_ZONE;
+  hour += TIME_ZONE;
 
-  if(hours == 3 && update == true) {
+  if(hour == 3 && update == true) {
     updateRTC();
     update == false;
   }
@@ -190,46 +190,46 @@ void getRTCTime(int &minutes, int &hours) {
   update == true;
 
   //checks if the timezone compensated time is outside of 24hr
-  if (hours < 0)
+  if (hour < 0)
   {
-    hours = 24 + hours;
+    hour = 24 + hour;
   }
-  else if (hours > 23)
+  else if (hour > 23)
   {
-    hours = hours - 24;
+    hour = hour - 24;
   }
 
   //converts to 12hr format
   #ifdef TWELVE_HOUR
-    twelveHour(hours);
+    twelveHour(hour);
   #endif
 
   return;
 }
 
 //converts 24hr time to 12hr time
-void twelveHour(int &hours) {
-  if(hours == 0)
+void twelveHour(int &hour) {
+  if(hour == 0)
     {
-      hours = 12;
+      hour = 12;
     }
-    else if(hours > 12)
+    else if(hour > 12)
     {
-      hours -= 12;
+      hour -= 12;
     }
 
   return;
 }
 
 void startup() {
-  int minutes, hours;
+  int minute, hour;
   int minOnes, minTens, hourOnes, hourTens;
   int i, delaySpeed = 50;
 
   //sets the RTC time using the GPS
   setRTCTime();
   //pulls the time from the RTC
-  getRTCTime(minutes, hours);
+  getRTCTime(minute, hour);
 
   //cycles through the digits of the tubes for show
   cycleDisplay(delaySpeed);
@@ -237,19 +237,19 @@ void startup() {
   //prints the time data
   #ifdef TIME_DEBUG
     Serial.print("Startup: ");
-    Serial.print(hours);
+    Serial.print(hour);
     Serial.print(":");
-    Serial.print(minutes);
+    Serial.print(minute);
     Serial.print("\n");
   #endif
 
   //parses the minute data for the tens and ones digits
-  minOnes = minutes % 10;
-  minTens = (minutes - minOnes) / 10;
+  minOnes = minute % 10;
+  minTens = (minute - minOnes) / 10;
 
   //parses the hour data for the tens and ones digits
-  hourOnes = hours % 10;
-  hourTens = (hours - hourOnes) / 10;
+  hourOnes = hour % 10;
+  hourTens = (hour - hourOnes) / 10;
 
   //cycles trough all the digits until the the correct number is found
   for(i = 0; i < 10; i++) {
@@ -300,22 +300,22 @@ void cycleDisplay(int &delaySpeed) {
   return;
 }
 
-//writes the given value to the minutes gpio expander
+//writes the given value to the minute gpio expander
 void nixieDisplayMinutes(int a, int b, int c, int d, int value) {
-  minutes.digitalWrite(d, (value & 0x08) >> 3);
-  minutes.digitalWrite(c, (value & 0x04) >> 2);
-  minutes.digitalWrite(b, (value & 0x02) >> 1);
-  minutes.digitalWrite(a, value & 0x01);
+  minute.digitalWrite(d, (value & 0x08) >> 3);
+  minute.digitalWrite(c, (value & 0x04) >> 2);
+  minute.digitalWrite(b, (value & 0x02) >> 1);
+  minute.digitalWrite(a, value & 0x01);
 
   return;
 }
 
-//writes the given value to the hours gpio expandr
+//writes the given value to the hour gpio expandr
 void nixieDisplayHours(int a, int b, int c, int d, int value) {
-  hours.digitalWrite(d, (value & 0x08) >> 3);
-  hours.digitalWrite(c, (value & 0x04) >> 2);
-  hours.digitalWrite(b, (value & 0x02) >> 1);
-  hours.digitalWrite(a, value & 0x01);
+  hour.digitalWrite(d, (value & 0x08) >> 3);
+  hour.digitalWrite(c, (value & 0x04) >> 2);
+  hour.digitalWrite(b, (value & 0x02) >> 1);
+  hour.digitalWrite(a, value & 0x01);
 
   return;
 }
