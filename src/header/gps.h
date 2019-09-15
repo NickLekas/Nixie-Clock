5031a0d476dba37e2a22d02718bb714d7be6d26c
NickLekas <apollo2.95@hotmail.com>
2019-09-14 23:06:01 -0700

    Added missing header file to gps.h

--- src/header/gps.h
+++ src/header/gps.h
@@ -4,6 +4,7 @@
 #include <Adafruit_GPS.h>
 
 #include "header/pins.h"
+#include "header/config.h"
 
 void gpsInit(); //initializes the gps module
 void getGPSTime(int &seconds, int &minute, int &hour); //reads the time data from the gps and applies the time zone
