5fb4a6c694b2077502525f3b068973b4fe9c9dfa
NickLekas <apollo2.95@hotmail.com>
2020-03-25 20:17:59 -0700

    added pcb pins and ic labels

--- include/pcbPins.h
+++ include/pcbPins.h
@@ -2,46 +2,46 @@
 #define PCBPINS_H
 
 //GPS pins
-#define gpsSerial Serial1
-const int gpsCtrl = 4;
+#define gpsSerial Serial3 //RX:PJ0 TX:PJ1
+const int gpsCtrl = 30; //PC7
 
 //dimming pins
-const int dim = 3;
-const int light = A0;
+const int dim = 4; //PG5
+const int light = A0; //PF0
 
 //dip switch pins
 //time set pins
-const int plusMinus = 5; //1
-const int eight = 6; //2
-const int four = 7; //3
-const int two = 8; //4
-const int one = 9; //5
-const int dst = 10; //6
-const int timeMode = 11; //7
-//extra pins allotted for latter expansion
-const int extra1 = 12;
-const int extra2 = 13;
+const int plusMinus = 6; //1 PH3
+const int eight = 7; //2 PH4
+const int four = 8; //3 PH5
+const int two = 9; //4 PH6
+const int one = 53; //5 PB0
+const int dst = 52; //6 PB1
+const int timeMode = 51; //7 PB2
+//extra pins allotted for latter potential expansion
+const int extra1 = 50; //PB3
+const int extra2 = 10; //PB4
 
 //digit pins
 //minutes ones
-const int minOnesA = 34;
-const int minOnesB = 35;
-const int minOnesC = 36;
-const int minOnesD = 37;
+const int minOnesA = 35; //PC2
+const int minOnesB = 37; //PC0
+const int minOnesC = 40; //PG1
+const int minOnesD = 36; //PC1
 //minutes tens
-const int minTensA = 30;
-const int minTensB = 31;
-const int minTensC = 32;
-const int minTensD = 33;
+const int minTensA = 41; //PG0
+const int minTensB = 18; //PD3
+const int minTensC = 19; //PD2
+const int minTensD = 38; //PD7
 //hours ones
-const int hrOnesA = 26;
-const int hrOnesB = 27;
-const int hrOnesC = 28;
-const int hrOnesD = 29;
+const int hrOnesA = 42; //PL7
+const int hrOnesB = 44; //PL5
+const int hrOnesC = 45; //PL4
+const int hrOnesD = 43; //PL6
 //hours tens
-const int hrTensA = 22;
-const int hrTensB = 23;
-const int hrTensC = 24;
-const int hrTensD = 25;
+const int hrTensA = 46; //PL3
+const int hrTensB = 48; //PL1
+const int hrTensC = 49; //PL0
+const int hrTensD = 47; //PL2
 
 #endif
