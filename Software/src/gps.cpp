7aa5196b180270b7cbf5ea9be1870c4ea6cd013f
NickLekas <apollo2.95@hotmail.com>
2021-05-26 16:54:24 -0700

    Started tracking the Software folder

---
+++ Software/src/gps.cpp
@@ -0,0 +1,78 @@
+#include "gps.h"
+
+Adafruit_GPS gps(&gpsSerial);                               // Creates the GPS object
+
+// Initializes the GPS module
+void gpsInit() {
+    pinMode(gpsCtrl, OUTPUT);                               // Sets the pin to turn the GPS on and off
+    gpsOn();                                                // Turns the GPS on
+
+    gps.begin(9600);                                        // Sets the GPS serial connection to a baudrate of 9600Hz
+    gps.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCONLY);          // Sets the GPS to send minimal data
+    gps.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);              // Sets the GPS to refresh at a rate of 1Hz
+
+    delay(1000);                                            // Wait 1 second for the GPS to save the settings
+
+    gpsOff();                                               // Turns the GPS off
+
+    return;
+}
+
+// Pulls time from the GPS satellites
+// Time is in 24Hr UTC format
+void getGPSTime(int &seconds, int &minute, int &hour) {
+    #ifdef GENERAL_DEBUG
+        Serial.println("Waiting for GPS fix");              // Prints the waiting for fix debug message          
+    #endif
+
+    do {                                                    // Checks the GPS for new data and loops until a fix is acquired
+        gps.read();                                         // Reads the most recent data from the GPS
+
+        if (gps.newNMEAreceived()) {                        // Checks if new data was received
+            gps.parse(gps.lastNMEA());                      // Parses the new data
+        }
+
+    #ifdef FIX_DEBUG                                        // Prints out the satellite fix data on one line to the serial monitor
+        Serial.print("Fix: ");
+        Serial.print(gps.fix);
+        Serial.print("\n");
+    #endif
+
+    } while(gps.fix < 1 && !gps.newNMEAreceived());         // Breaks the loop once a fix is acquired
+
+    #ifdef GENERAL_DEBUG                                    
+        Serial.println("GPS fix found");                    // Indacates when a satellite fix is acquired
+    #endif
+
+    minute = gps.minute;                                    // Writes the current minute from the GPS time to the variable 'minute'
+    hour = gps.hour;                                        // Writes the current hour from the GPS time to the variable 'hour'
+    seconds = gps.seconds;                                  // Writes the current second from the GPS time to the variable 'seconds'
+
+    #ifdef TIME_DEBUG                                       // Prints the the time from the GPS to the serial monitor in the standard hh:mm:ss format
+        Serial.print("GPS: ");
+        Serial.print(hour);
+        Serial.print(":");
+        Serial.print(minute);
+        Serial.print(":");
+        Serial.print(seconds);
+        Serial.print("\n");
+    #endif
+
+    gpsOff();                                               // Turns off the GPS
+
+    return;
+}
+
+// Turns the GPS on
+void gpsOn() {
+    digitalWrite(gpsCtrl, true);                            // Sets the GPS control pin to high
+
+    return;
+}
+
+// Turns the GPS off
+void gpsOff() {
+    digitalWrite(gpsCtrl, false);                           // Sets the GPS control pin to low
+
+    return;
+}
---
+++ Software/src/rtc.cpp
@@ -0,0 +1,110 @@
+#include "rtc.h"
+
+RTC_DS3231 rtc;                                                 // Initializes the RTC object
+
+// Enables the RTC and checks for power loss
+// If there was a power loss, sets time using the GPS
+// If no power loss, useses last know time and will be updated at 3am local time
+void RTCInit() {
+    rtc.begin();                                                // Initializes the RTC
+
+    if(rtc.lostPower()) {                                       // Checks if the RTC lost power
+        #ifdef TIME_DEBUG                                       // Prints that power was lost to the serial monitor
+            Serial.println("power loss");
+        #endif
+        setRTCTime();                                           // Sets the RTC time using the GPS
+    }
+
+    return;
+}
+
+// RTC time is stored as 24hr UTC time
+void setRTCTime() {
+    int seconds, minute, hour;
+
+    gpsOn();                                                    // Turns on the GPS
+
+    getGPSTime(seconds, minute, hour);                          // Gets the current time from the GPS
+
+    rtc.adjust(DateTime(1970, 1, 1, hour, minute, seconds));    // Stores the GPS time in the RTC, sets the date to the start of UNIX time since the data isn't needed
+
+    return;
+}
+
+// Gets the time from the RTC and converts it to the the users time zone and to 12 Hr formtat if selected
+void getRTCTime(int &minute, int &hour) {
+    int timeZone;
+
+    DateTime now = rtc.now();                                   // Gets the current time from the RTC
+
+    minute = now.minute();                                      // Gets the current minute from the fetched RTC data
+    hour = now.hour();                                          // Gets the current hour from the fetched RTC data
+
+    readSettings(timeZone);                                     // Reads the switch settings for the set time zone
+
+    hour += timeZone;                                           // Applies the time zone value to the hour
+    
+    if(hour < 0) {                                              // Checks if the adjusted time is less than 24
+        hour += 24;                                             // If the adjusted time is less than 0, add 24                
+    }
+    else if(hour > 23) {                                        // Checks if the adjusted time is greater than 24
+        hour -= 24;                                             // If the adjusted time is greater than 24, subtract 24
+    }
+
+    if(digitalRead(timeMode) == LOW) {                          // Checks if the 12 hour switch is toggled
+        twelveHour(hour);                                       // Converts the time to 12 Hr format
+    }
+
+    return;
+}
+
+// Updates the RTC time using the GPS and maintains the tubes
+// This will occur every day at 3am local time when the user is likely asleep
+void updateRTC() {
+    int cycleSpeed = 50;
+
+    cycleDisplay(cycleSpeed);                                   // Cycles the display so all digets get used once each day to extend their life
+
+    setRTCTime();                                               // Updates the RTC using the GPS
+
+    return;
+}
+
+//converts 24hr time to 12hr time
+void twelveHour(int &hour) {
+    if(hour == 0) {                                             // Checks if the hour is 0
+        hour = 12;                                              // Sets the hour to 12 if it's 0
+    }
+    else if(hour > 12) {                                        // Checks if the hour is greater than 12
+        hour -= 12;                                             // Subtracts 12 from the hour if it's greater than 12
+    }
+
+    return;
+}
+
+// Reads settings switches 3-7 as 5-bit signed binary to set the time zone
+// Reads each bit and sums the decimal values to set the time zone
+void readSettings(int &timeZone) {
+
+    if(digitalRead(plusMinus) == HIGH){                         // Reads each switch one at a time                    
+        timeZone += -16;
+    }
+    if(digitalRead(eight) == LOW) {
+        timeZone += 8;
+    }
+    if(digitalRead(four) == LOW) {
+        timeZone += 4;
+    }
+    if(digitalRead(two) == LOW) {
+        timeZone += 2;
+    }
+    if(digitalRead(one) == LOW) {
+        timeZone += 1;
+    }
+
+    if(digitalRead(dst) == LOW) {                               // Reads dip switch 1 to add an hour for daylight savings time
+        timeZone++;
+    }
+
+    return;
+}
